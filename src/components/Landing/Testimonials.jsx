0edbf4028880770102168ec4a58517f4e712d2e2
Sanskar Gupta <72657115+sanskar0901@users.noreply.github.com>
2023-03-14 08:36:55 +0530

    horizontal scroll fixed

--- src/components/Landing/Testimonials.jsx
+++ src/components/Landing/Testimonials.jsx
@@ -6,19 +6,19 @@ import testimonial4 from "../../assets/testimonial4.png";
 
 const Testimonials = () => {
   return (
-    <section className="bg-gray-50 text-xl text-black w-[100vw] text-center h-auto border-t-4 mx-5 border-black flex flex-col items-center justify-start py-5 lg:mt-5 mt-[35rem] py-10 ">
+    <section className="bg-gray-50 text-xl text-black w-full text-center h-auto border-t-4 mx-5 border-black flex flex-col items-center justify-start py-5 lg:mt-5 mt-[35rem] py-10 ">
       <h1 className="font-bold text-3xl sm:text-5xl">What clients say about us!</h1>
       <div className="flex items-center justify-evenly w-full py-5  flex-wrap gap-10 scale-[85%]  ">
         <div className="flex w-[27rem] p-5 border-[0.2rem] border-black text-center justify-center items-center">
           <img className="w-32 h-32" src={testimonial1} alt="testimonial1" />
           <h2 className="font-bold  text-xl">
-          "Their teams are expert in matching you with the right provider"
+            "Their teams are expert in matching you with the right provider"
           </h2>
         </div>
         <div className="flex w-[27rem] p-5 border-[0.2rem] border-black text-center justify-center items-center">
           <img className="w-32 h-32" src={testimonial2} alt="testimonial1" />
           <h2 className="font-bold  text-xl">
-          "Best service providers. Great experience! "
+            "Best service providers. Great experience! "
           </h2>
         </div>
       </div>
@@ -26,13 +26,13 @@ const Testimonials = () => {
         <div className="flex w-[27rem] p-5 border-[0.2rem] border-black text-center justify-center items-center">
           <img className="w-32 h-32" src={testimonial3} alt="testimonial1" />
           <h2 className="font-bold  text-xl">
-          "They are probably the best in the market "
+            "They are probably the best in the market "
           </h2>
         </div>
         <div className="flex w-[27rem] p-5 border-[0.2rem] border-black text-center justify-center items-center">
           <img className="w-32 h-32" src={testimonial4} alt="testimonial1" />
           <h2 className="font-bold  text-xl">
-          "On-time services and with 100% accuracy."
+            "On-time services and with 100% accuracy."
           </h2>
         </div>
       </div>
