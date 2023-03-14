250274c04477c46292170b96bb0992b0fd8da654
Sanskar Gupta <72657115+sanskar0901@users.noreply.github.com>
2023-03-14 08:27:42 +0530

    file structure changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import Navigation from "./navigation";
 import './App.css'
-import ExpertiseComp from './components/expetise/ExpertiseComp'
+// import ExpertiseComp from './components/expetise/ExpertiseComp'
 // import NavbarComp from './components/Navbar/NavbarComp'
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       {/* <NavbarComp /> */}
-      
+
       <Navigation />
     </div>
   )
--- src/components/Landing/LandingComp.jsx
+++ src/components/Landing/LandingComp.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import NavbarComp from '../Navbar/NavbarComp'
 import Testimonials from "./Testimonials";
 import NewsLetter from "./NewsLetter";
-import ExpertiseComp from "../expetise/ExpertiseComp";
-import Carousels from "../Featured/Carousel";
+import ExpertiseComp from "./ExpertiseComp";
+import Carousels from "./Carousel";
 function LandingComp() {
   return (
     <div className="flex flex-col items-center justify-center  ">
       <NavbarComp />
       <Carousels />
-      <ExpertiseComp/>
-      <Testimonials/>
-      <NewsLetter/>
+      <ExpertiseComp />
+      <Testimonials />
+      <NewsLetter />
     </div>
   );
 }
